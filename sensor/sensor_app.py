2727aa2cbab839d0e31fa24ff20ecb8f72da207d
trickster000 <beniaminradu@ymail.com>
2022-09-15 14:37:08 +0300

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -4,7 +4,8 @@ from load_data import load_sensor_data
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
-
+data = []
+print("Sensor Data App")
 
 data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
