ae6528680d90e82156df33b4932d01b4a6556338
trickster000 <beniaminradu@ymail.com>
2022-09-19 16:38:56 +0300

    Added

---
+++ sensor/particle_count_info.py
@@ -0,0 +1,9 @@
+from house_info import HouseInfo
+
+
+class ParticleData(HouseInfo):
+  def _convert_data(self,data):
+    recs=[]
+    for rec in data:
+        recs=float(rec)
+    return recs     
